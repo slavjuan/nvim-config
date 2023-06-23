a06f24f64132028b911b56323977cbe7c893ee75
slavjuan <koenbeerta@gmail.com>
2023-06-23 13:56:33 +0100

    more on_attach functions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,8 +39,11 @@ local opts = { noremap = true, silent = true }
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local on_attach = function (client, bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition")
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()")
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()")
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 end
 
 for _, lsp in pairs(servers) do
